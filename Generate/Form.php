54f23a6ccdc7a05d0c917a91e591e6ee683e4701
Tam Bieszczad <tsb@warrior-poet.org>
2021-02-25 15:26:49 -0600

    Another fix attempt on replacing null id in Form addField.

--- Generate/Form.php
+++ Generate/Form.php
@@ -361,9 +361,11 @@ class Generate_Form
      */
     public function addField(array $fieldInfo){
         $_fs = $fieldInfo['fieldset'] ?? count($this->fieldset);
-//        $_fieldset = $this->fieldset[$_fs];
-//        $_fieldNum = is_array($_fieldset->field) ? count($_fieldset->field) : 0;
-//        $fieldInfo['id'] = !empty($fieldInfo['id']) ? $fieldInfo['id'] : 'field-' . $_fs . '-' . ++$_fieldNum;
+        if(empty($fieldInfo['id'])){
+            $_fieldset = $this->fieldset[$_fs];
+            $_fieldNum = is_array($_fieldset->field) ? count($_fieldset->field) : 0;
+            $fieldInfo['id'] = $fieldInfo['name'] ?? 'field-' . $_fs . '-' . ++$_fieldNum;
+        }
         $this->fieldset[$_fs]->addField($fieldInfo);
         //If this field has a name associated, add it to the _fieldlist array.
         if(!empty($fieldInfo['name'])){
